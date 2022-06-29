3bb9b59ef344eb480f6c7239b5de8edceba8e407
maximelombardo <maxime.77610@gmail.com>
2022-06-29 15:27:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+import sys
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.setGeometry(300, 200, 800, 300)
+
+
+
+
+
+
+window.show()
+sys.exit(app.exec())
